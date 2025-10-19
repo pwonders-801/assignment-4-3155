9d160b87175aa2a420602092d7ba1bdd67716b04
pwonders-801 <pwonders@charlotte.edu>
2025-10-19 13:45:45 -0400

    feat: Add Item class

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
+
     return {"item_id": item_id, "q": q}
